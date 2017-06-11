67d1916402697cd1747cac2f09bfcc3cff06016b
SkiboPavlo <skibo@i.ua>
2017-06-11 19:06:36 +0300

    created class car and his methods

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
 # exercise object model
-module ObjectModel
+module LinuxFriendly
+  def fork_process
+    super
+  end
+
+  def mine_bitcoins
+    inspect
+  end
+end
+
+# include module
+class Dell
+  include LinuxFriendly
+
+  def fork_process
+    p 'Parent: allocate memory'
+  end
+end
+
+# inheritance from Laptop
+class Laptop < Dell
+  prepend LinuxFriendly
+end
+
+# inheritance from Laptop
+class ObjectModel < Laptop
+  include LinuxFriendly
 end
--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -2,4 +2,27 @@
 
 # exercise state and behaviour
 module StateAndBehaviour
+  # base class
+  class Car
+    attr_accessor :year, :color, :model, :current_speed
+
+    def self.default_car
+      new
+    end
+
+    def initialize(car_attributes)
+      self.year = car_attributes[:year] || 2016
+      self.color = car_attributes[:color] || 'red'
+      self.model = car_attributes[:model] || 'impreza'
+      self.current_speed = 0
+    end
+
+    def speed_up(faster)
+      self.current_speed += faster
+    end
+
+    def push_break(lower)
+      self.current_speed -= lower if @current_speed >= lower
+    end
+  end
 end
